79d9acbb805bd8f06ce067d56a7dd0068ff0ec4a
Brenavis <97899072+chulokm@users.noreply.github.com>
2026-07-08 15:49:57 +0800

    Update st1.py

--- st1.py
+++ st1.py
@@ -10,7 +10,10 @@ st.divider()
 st.markdown(":blue-badge[Captain on the bridge]")#标记
 #st.badge("Captain on the bridge", color="blue")#标记
 st.caption("舰长已抵达舰桥")#注释
-df = 
+df = pd.DataFrame({
+    "运行人":["重装小兔","Bronya"],
+    "身份":["协同者","BOSS"]})
+st.dataframe(df)
 img = Image.open(r"D:\图片\lvo\XT.jpg")#绝对路径获取
 click_js = """
  <script>
@@ -24,6 +27,7 @@ placeholder = st.empty()#把开始播放藏在按钮里
 with placeholder:
      if st.button(""):
          st.audio(r"D:\音乐\HOYO-MiX - Gion2.flac", loop=True, autoplay=True)
+st.markdown(":blue-badge[----点击方块循环护肝的小曲----]")
 st.image(
     img,
     caption="正在工作-----",
