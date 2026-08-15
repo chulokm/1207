56e4768c3b7c4f094de0076b7efcfe7769a25da5
Brenavis <97899072+chulokm@users.noreply.github.com>
2026-08-15 18:39:01 +0800

    Update st1.py

--- st1.py
+++ st1.py
@@ -572,4 +572,4 @@ else:
                 except Exception as e:
                     loading_placeholder.empty()
                     st.error(f"⚠️{st.session_state.nick_name}失去讯号中......:{e}")
-                        print("capture error",e)
+                    print("capture error",e)
